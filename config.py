ab92c8c6ab6c6f0bc9f4cd701b6b994893cafd8e
Luca Fenu <luca.fenu01@gmail.com>
2023-07-28 22:46:05 +0200

    Added correct default endpoint

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ botToken = "12345:etcetcetc"
 chatID = 0
 
 # If you want to upload files bigger than 50Mb, you have to set a custom endpoint
-endpoint = "https://api.telegram.org/"
+endpoint = "https://api.telegram.org/bot"
